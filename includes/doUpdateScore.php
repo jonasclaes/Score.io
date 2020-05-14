a6036e08831685c0a270cb61c5a2341bd72f00df
unknown <gijsverjans@hotmail.be>
2020-05-14 14:11:36 +0200

    Revert "add update function"
    This reverts commit c4b61adc9355623a86d736f2dc269e824fe3ff27.

--- includes/doUpdateScore.php
+++ includes/doUpdateScore.php
@@ -1,17 +1,2 @@
 <?php
-$ID = $_POST['ID'];
-$timestamp = $_POST['timestamp'];
-$name = $_POST['name'];
-$destription = $_POST['destription'];
-$score = $_POST['score'];
-$maxScore = $_POST['maxScore'];
-
-include 'db.php';
-$sql = "UPDATE scores SET timestamp = '$timestamp', name = '$name', description = '$description', score = '$score', maxScore = '$maxScore' WHERE id = '$id';"
-if (mysqli_query($mysqli, $sql)) {
-    echo json_encode('Succes, score is geüpdate.');
-}
-else {
-    echo json_encode("Er ging iets fout.")
-}
-?>
+// Hier komt jouw code.
