5c42a73cf9162c69c67f6225aefcd6d85b7ee2ed
Jonas Claes <jonasclaes2001@outlook.com>
2020-05-14 16:26:53 +0200

    Fixed get user

--- includes/doGetUser.php
+++ includes/doGetUser.php
@@ -1,13 +1,11 @@
 <?php
 
-    $username = $_SESSION["username"];
+$username = $_SESSION["username"];
 
-    $result = $mysqli->query("SELECT * FROM users WHERE username = '$username'");
+$result = $mysqli->query("SELECT * FROM users WHERE username = '$username'");
 
-    if ($result->num_rows === 0) {
-        return;
-    }
+if ($result->num_rows === 0) {
+    return;
+}
 
-    $user = $result->fetch_assoc();
-
-    echo $user["password"];
+$user = $result->fetch_assoc();
