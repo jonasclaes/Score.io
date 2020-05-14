875b7b583e0ae1245994746e56f25b96671f1875
Warre Jansen <warrejansen@gmail.com>
2020-05-14 13:44:37 +0200

    add score function

--- includes/doInsertScore.php
+++ includes/doInsertScore.php
@@ -1,2 +1,21 @@
 <?php
-// Hier komt jouw code.
+
+$timestamp = $_POST['timestamp']
+$name = $_POST['name']
+$description = $_POST['description']
+$score = $_POST['score']
+$maxScore = $_POST['maxScore']
+
+
+include "db.php";
+$sql = "INSERT INTO scores (id, timestamp, name, description, score, maxScore) VALUES (NULL, '$timestamp', '$name', '$description', '$score', '$maxScore')";
+if (musqli_query($mysqli, $sql)) {
+    echo json_encode('Score toegevoegd');
+}
+else{
+    echo json_encode('Error');
+
+}
+?>
+
+
